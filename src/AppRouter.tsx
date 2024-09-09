ae846a420828f2c5caa4db7f3cadd6656c5376f9
John Omoseni <johnnyomoseni100@gmail.com>
2024-09-09 22:50:30 +0100

    chore: edited app router

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./app/(website1)/(home)/page";
 import About from "./app/(website1)/about/page";
 import GetStarted from "./app/(website1)/get-started/page";
+import GetStartedSuccess from "./app/(website1)/get-started/success/page";
 import FAQs from "./app/(website1)/faqs/page";
 import Contact from "./app/(website1)/contact/page";
 import LayoutProvider from "./providers/LayoutProvider";
@@ -14,9 +15,14 @@ const AppRouter = () => {
 			<Routes>
 				<Route element={<LayoutProvider />}>
 					<Route path="/" element={<Home />} />
+					<Route path="*" element={<Home />} />
+
 					<Route path="/about" element={<About />} />
 					<Route path="/contact" element={<Contact />} />
-					<Route path="/get-started" element={<GetStarted />} />
+					<Route path="/get-started">
+						<Route index element={<GetStarted />} />
+						<Route path="success" element={<GetStartedSuccess />} />
+					</Route>
 					<Route path="/faqs" element={<FAQs />} />
 				</Route>
 			</Routes>
